999685f3a8867f2d4a3ec131d2197813a20ceb03
soso シ <80224791+disco-io@users.noreply.github.com>
2022-07-29 18:11:42 -0400

    Update Torchic.java

--- codes/Torchic.java
+++ codes/Torchic.java
@@ -3,30 +3,11 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
 
-public class Torchic
+public class Torchic extends Pokemon
 {
-  //fields:
-  public static final int FRAME = 400;
-  
-  private Image trch;
-  private BufferedImage myImage;
-
   //constructors:
   public Torchic()
   {
-    trch = (new ImageIcon("torchic.png")).getImage();
-  }
-
-  //instance methods:
-  public void draw(Graphics myBuffer, int x, int y, int size, boolean right)
-  {
-    if (right)
-    {
-      myBuffer.drawImage(trch, x, y, size, size, null, null);  
-    }
-    else
-    {
-       myBuffer.drawImage(trch, x+size, y-size, size, size, null, null); 
-    }
+    myImage = (new ImageIcon("torchic.png")).getImage();
   }
 }
